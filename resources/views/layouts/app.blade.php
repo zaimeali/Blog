d737e97e2ef7e8607b05dabdee053f1c36c94a7e
zaimeali <zaimeali1997@gmail.com>
2020-09-18 17:10:23 +0500

    very nice

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,1 +1,15 @@
-nice
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
